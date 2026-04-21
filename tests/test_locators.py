1731119aa3aafe4ce6b6ab3b77082949cbca3409
Djekaizfeo <djekaizfeo-05@mail.ru>
2026-04-21 15:35:02 +0200

    add new locators tests

--- tests/test_locators.py
+++ tests/test_locators.py
@@ -7,3 +7,23 @@ def test_locator_role(page: Page):
     expect(page).to_have_title("Best books of 2026 – read online for free or download in fb2")
     page.get_by_role("button", name="Catalog").click()
     expect(page.get_by_text("Parenting", exact=True)).to_be_visible()
+
+def test_locator_placeholder(page: Page):
+    page.goto("https://litres.com/")
+    page.get_by_placeholder("Find").fill("game of thrones")
+    page.keyboard.press("Enter")
+    expect(page.get_by_text("Search results «game of thrones»", exact=True)).to_be_visible()
+
+def test_locator_testid(page: Page):
+    page.goto("https://litres.com/")
+    page.get_by_test_id("dropDown__sorting--wrapper").click()
+    expect(page.get_by_text("Русский", exact=True)).to_be_visible()
+
+def test_locator_alttext(page: Page):
+    page.goto("https://litres.com/audiobooks/")
+    page.get_by_alt_text("Litres logo").click()
+    expect(page).to_have_url("https://litres.com/")
+
+def test_locator_xpath(page: Page):
+    page.goto("https://litres.com/")
+    expect(page.locator("xpath=//a[@href='/pages/litres_oferta/']")).to_be_visible()
